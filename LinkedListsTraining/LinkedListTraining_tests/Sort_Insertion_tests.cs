1ee9b5d316bde5a680dee082fa7d3df0176814de
Kyle Melton <87396239+KyleMel@users.noreply.github.com>
2021-09-27 14:02:44 -0500

    start of project

--- LinkedListsTraining/LinkedListTraining_tests/Sort_Insertion_tests.cs
+++ LinkedListsTraining/LinkedListTraining_tests/Sort_Insertion_tests.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 namespace LinkedListTraining_tests
 {
     [TestClass]
-    public class Sort_tests
+    public class Sort_Insertion_tests
     {
         [TestMethod]
         public void Test_Can_Fail()
@@ -19,98 +19,98 @@ namespace LinkedListTraining_tests
         }
 
         [TestMethod]
-        public void Bubble_Empty_ReturnsEmpty()
+        public void Insertion_Empty_ReturnsEmpty()
         {
             var arr = new int[] { };
 
-            Sort.Bubble(arr);
+            Sort.Insertion(arr);
 
             Assert.AreEqual(arr.Length, 0);
         }
 
         [TestMethod]
-        public void Bubble_OneItem_ReturnsItem()
+        public void Insertion_OneItem_ReturnsItem()
         {
             var arr = new int[] { 1 };
 
-            Sort.Bubble(arr);
+            Sort.Insertion(arr);
 
             Assert.AreEqual(arr.Length, 1);
         }
 
         [TestMethod]
-        public void Bubble_TwoItems_ReturnsItemsSorted()
+        public void Insertion_TwoItems_ReturnsItemsSorted()
         {
             var arr = new int[] { 2, 1 };
             var expected = new int[] { 1, 2 };
 
-            Sort.Bubble(arr);
+            Sort.Insertion(arr);
 
             CollectionAssert.AreEqual(expected, arr);
         }
 
         [TestMethod]
-        public void Bubble_OddAmountOfItems_ReturnsItemsSorted()
+        public void Insertion_OddAmountOfItems_ReturnsItemsSorted()
         {
-            var arr = new int[] { 2, 1 , 4 , 8 , 9 };
+            var arr = new int[] { 2, 1, 4, 8, 9 };
             var expected = new int[] { 1, 2, 4, 8, 9 };
 
-            Sort.Bubble(arr);
+            Sort.Insertion(arr);
 
             CollectionAssert.AreEqual(expected, arr);
         }
 
         [TestMethod]
-        public void Bubble_EvenAmountOfItems_ReturnsItemsSorted()
+        public void Insertion_EvenAmountOfItems_ReturnsItemsSorted()
         {
             var arr = new int[] { 2, 1, 4, 8, 9, 6 };
             var expected = new int[] { 1, 2, 4, 6, 8, 9 };
 
-            Sort.Bubble(arr);
+            Sort.Insertion(arr);
 
             CollectionAssert.AreEqual(expected, arr);
         }
 
         [TestMethod]
-        public void Bubble_ReversedList_ReturnsItemsSorted()
+        public void Insertion_ReversedList_ReturnsItemsSorted()
         {
             var arr = new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
             var desired = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-            Sort.Bubble(arr);
+            Sort.Insertion(arr);
 
             CollectionAssert.AreEqual(arr, desired);
         }
 
         [TestMethod]
-        public void Bubble_SimilarItemList_ReturnsItemsSorted()
+        public void Insertion_SimilarItemList_ReturnsItemsSorted()
         {
             var arr = new int[] { 5, 6, 5, 6, 5, 6, 5, 6 };
             var desired = new int[] { 5, 5, 5, 5, 6, 6, 6, 6 };
 
-            Sort.Bubble(arr);
+            Sort.Insertion(arr);
 
             CollectionAssert.AreEqual(arr, desired);
         }
 
         [TestMethod]
-        public void Bubble_SpecialMergeSortNumberList_ReturnsItemsSorted()
+        public void Insertion_SpecialMergeSortNumberList_ReturnsItemsSorted()
         {
             var arr = new int[] { 38, 27, 43, 3, 9, 82, 10 };
             var desired = new int[] { 3, 9, 10, 27, 38, 43, 82 };
 
-            Sort.Bubble(arr);
+            Sort.Insertion(arr);
 
             CollectionAssert.AreEqual(arr, desired);
         }
 
         [TestMethod]
-        public void Bubble_ListWithBounds_ReturnsItemsSorted()
+        public void Insertion_ListWithBounds_ReturnsItemsSorted()
         {
             var arr = new int[] { 99999, Int32.MaxValue, Int32.MinValue, 10, 44, -1111, 0, 2, 2, 2, -1 };
-            var desired = new int[] { Int32.MinValue, - 1111, -1, 0, 2, 2, 2, 10, 44, 99999, Int32.MaxValue };
+            var desired = new int[] { Int32.MinValue, -1111, -1, 0, 2, 2, 2, 10, 44, 99999, Int32.MaxValue };
 
-            Sort.Bubble(arr);
+            Sort.Insertion(arr);
 
             CollectionAssert.AreEqual(arr, desired);
         }
--- LinkedListsTraining/LinkedListsTraining/Sort.cs
+++ LinkedListsTraining/LinkedListsTraining/Sort.cs
@@ -10,22 +10,105 @@ namespace LinkedListsTraining
     {
         public static void Bubble(int[] arr)
         {
-            throw new NotImplementedException();
+            for (var i = 0; i < arr.Length - 1; i++)
+            {
+                for (var k = 0; k < arr.Length - 1 - i; k++)
+                {
+                    if (arr[k] > arr[k + 1])
+                    {
+                        var temp = arr[k];
+                        arr[k] = arr[k + 1];
+                        arr[k + 1] = temp;
+                    }
+                }
+            }
         }
 
         public static void Selection(int[] arr)
         {
-            throw new NotImplementedException();
+            int smallest;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                smallest = i;
+                for (int k = i + 1; k < arr.Length; k++)
+                {
+                    if (arr[k] < arr[smallest])
+                    {
+                        smallest = k;
+                    }
+                }
+                int temp = arr[smallest];
+                arr[smallest] = arr[i];
+                arr[i] = temp;
+            }
         }
-
         public static void Insertion(int[] arr)
         {
-            throw new NotImplementedException();
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int val = arr[i];
+                //int flag = 0;
+                for (int j = i - 1; j >= 0 && /*flag != 1*/;)
+                {
+                    if (val < arr[j])
+                    {
+                        arr[j + 1] = arr[j];
+                        j--;
+                        arr[j + 1] = val;
+                    }
+                    //else flag = 1;
+                }
+            }
         }
-
-        public static void MergeSort(int[] arr)
+        public static int[] MergeSort(int[] arr)
         {
-            throw new NotImplementedException();
+            if (arr.Length <= 1) return arr;
+            decimal midpoint = arr.Length / 2;
+            var mid = Convert.ToInt32(Math.Ceiling(midpoint));
+
+            var bot = arr.Take(mid).ToArray();
+            var top = arr.Skip(mid).ToArray();
+
+            var bottomOut = MergeSort(bot);
+            var topOut = MergeSort(top);
+
+            var combine = Combine(bottomOut, topOut);
+
+            return combine;
+        }
+        private static int[] Combine(int[] bottom, int[] top)
+        {
+            var i = 0; //bottom
+            var j = 0; //top
+            var k = 0;
+            var arr = new int[bottom.Length + top.Length];
+
+            while (i < bottom.Length && j < top.Length)
+            {
+                if(bottom[i] < top[j])
+                {
+                    arr[k] = bottom[i];
+                    i++;
+                }    
+                else {
+                    arr[k] = top[j];
+                    j++;
+                }
+                k++;
+            }  
+            while (i < bottom.Length)
+            {
+                arr[k] = bottom[i];
+                i++;
+                k++;
+            }
+            while (j < top.Length)
+            {
+                arr[k] = top[j];
+                j++;
+                k++;
+            }
+            return arr;
         }
     }
 }

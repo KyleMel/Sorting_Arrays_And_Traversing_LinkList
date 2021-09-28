a780e75449318bef296b9b40cfa8c05f13053afc
Kyle Melton <87396239+KyleMel@users.noreply.github.com>
2021-09-27 20:46:31 -0500

    MyLinkedList Class update methods, AddAtTail, AddAtIndex, DeleteAtIndex

--- LinkedListsTraining/LinkedListTraining_tests/MyLinkedList_tests.cs
+++ LinkedListsTraining/LinkedListTraining_tests/MyLinkedList_tests.cs
@@ -15,12 +15,54 @@ namespace LinkedListTraining_tests
         public void AddAtHead_OneNumber_CanReturnNumber()
         {
             var input = 2;
-            var expected = 0;
+            var expected = 2;
             var sut = new MyLinkedList();
 
             sut.AddAtHead(input);
 
             var actual = sut.Get(0);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void AddAtTail_OneNumber_CanReturnNumber()
+        {
+            var input = 6;
+            var expected = 6;
+            var sut = new MyLinkedList();
+
+            sut.AddAtTail(input);
+
+            var actual = sut.Get(0);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void AddAtIndex_OneNumber_CanReturnNumber()
+        {
+            var index = 2;
+            var valueToAdd = 3;
+
+            var expected = (2, 3);
+            var sut = new MyLinkedList();
+
+            sut.AddAtIndex(index, valueToAdd);
+
+            var actual = sut.Get(0);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void DeleteAtIndex_OneNumber_CanReturnNumber()
+        {
+            var index = 1;
+            var valueToDelete = 1;
+
+            var expected = (0);
+            var sut = new MyLinkedList();
+
+            sut.AddAtIndex(index, valueToDelete);
+
+            var actual = sut.Get(0);
+
             Assert.AreEqual(expected, actual);
         }
     }
--- LinkedListsTraining/LinkedListsTraining/MyLinkedList.cs
+++ LinkedListsTraining/LinkedListsTraining/MyLinkedList.cs
@@ -9,23 +9,26 @@ namespace LinkedListsTraining
     public class MyLinkedList
     {
         public ListNode Head { get; set; }
+        public ListNode Tail { get; set; }
+        public int Count { get; set; }
 
         /** Initialize your data structure here. */
         public MyLinkedList()
         {
-            int[] numbers = { 1, 2, 3, 4, 5 };
-        }   
-
+            Head = null;
+            Tail = null;
+            Count = 0;
+        }
         /** Get the val of the index-th node in the linked list. If the index is invalid, return -1. */
         public int Get(int index)
         {
-            if (index > 0) return -1;
+            if (index > Count) return -1;
+            ListNode temp = Head;
             for (int i = 0; i < index; i++)
             {
-                Console.WriteLine(i);
+                temp = temp.next;
             }
-            return index;
-
+            return temp.val;
         }
 
         /** Add a node of val val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
@@ -34,25 +37,92 @@ namespace LinkedListsTraining
             ListNode newNode = new ListNode(val);
             newNode.next = Head;
             Head = newNode;
+            Count++;
+
+            if (Head.next == null)
+            {
+                Tail = Head;
+            }
         }
 
         /** Append a node of val val to the last element of the linked list. */
         public void AddAtTail(int val)
         {
-            throw new NotImplementedException();
+            ListNode newNode = new ListNode(val);
+            if (Tail != null)
+            {
+                Tail.next = newNode;
+            }
+            else if (Tail == null || Count == 0)
+            {
+                Head = newNode;
+            }
+            Tail = newNode;
+            Count++;
         }
 
-        /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
+        /** Add a node of value val before the index-th node in the linked list. 
+         * If index equals to the length of linked list, the node will be appended 
+         * to the end of linked list. If index is greater than the length, the node 
+         * will not be inserted. */
         public void AddAtIndex(int index, int val)
         {
-            throw new NotImplementedException();
+            if (index < 0 || index > Count) return;
+            if (index == 0)
+            {
+                AddAtHead(val);  
+            }
+            else if (index == Count)
+            {
+                AddAtTail(val);
+            }
+            else
+            {
+                ListNode temp = Head;
+                for (int i = 0; i < index - 1; i++)
+                {
+                    temp = temp.next;
+                }
+                ListNode n = new ListNode(val);
+                ListNode save = temp;
+                temp = temp.next;
+                save.next = n;
+                n.next = temp;
+                Count++;
+            }
         }
 
         /** Delete the index-th node in the linked list, if the index is valid. */
         public void DeleteAtIndex(int index)
         {
-            throw new NotImplementedException();
+            if (Count == 0 || index < 0 || index >= Count)
+            {
+                return;
+            }
+            if (index == 0)
+            {
+                Head = Head.next;
+                Count--;
+                if (Count == 0)
+                {
+                    Tail = null;
+                }
+            }
+            else
+            {
+                ListNode newNode = Head;
+                for (int i = 0; i < index - 1; i++)
+                {
+                    newNode = newNode.next;
+                }
+                newNode.next = newNode.next.next;
+                Count--;
+                if (newNode.next == null)
+                {
+                    Tail = newNode;
+                }
 
+            }
         }
     }
 }
